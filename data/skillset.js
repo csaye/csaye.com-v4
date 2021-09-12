07ed7388d3a26e9626767fa5396a0ce8e85e6737
Cooper Saye <coopersaye@gmail.com>
2021-09-11 23:55:28 -0700

    Import skillset data.

---
+++ data/skillset.js
@@ -0,0 +1,59 @@
+export const skillset = [
+  {
+    title: "Languages",
+    icon: <i className="fas fa-code" />,
+    icons: [
+      "C#",
+      "JavaScript",
+      "Python",
+      "Java",
+      "C++",
+      "Bash",
+      "HTML",
+      "CSS",
+      "Sass",
+      "TypeScript",
+      "CoffeeScript",
+      "Node.js",
+      "JSX"
+    ]
+  },
+  {
+    title: "Software",
+    icon: <i className="fas fa-laptop" />,
+    icons: [
+      "Unity",
+      "Aseprite",
+      "FL Studio",
+      "VSCode",
+      "Visual Studio",
+      "Atom",
+      "IDLE",
+      "Audacity",
+      "BFXR",
+      "GIMP",
+      "Brackets",
+      "Blender",
+      "Trello"
+    ]
+  },
+  {
+    title: "Tools",
+    icon: <i className="fas fa-tools" />,
+    icons: [
+      "React",
+      "Next.js",
+      "Firebase",
+      "MonoGame",
+      "Netlify",
+      "Vercel",
+      "Git",
+      "GitHub",
+      "Angular",
+      "Shell",
+      "Pygame",
+      "Command Line",
+      "Heroku"
+    ]
+  }
+];
