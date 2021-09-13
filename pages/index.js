2ce320aee5262175016b86dc816a55d056f30724
Cooper Saye <coopersaye@gmail.com>
2021-09-12 21:25:07 -0700

    Populate homepage.

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,16 @@
+import Contact from '../components/Contact.js';
+import Action from '../components/Action.js';
+import Projects from '../components/Projects.js';
+
 import styles from '../styles/pages/Index.module.css';
 
 export default function Index() {
   return (
     <div>
+      <h1>Cooper Saye</h1>
+      <Contact />
+      <Action />
+      <Projects />
     </div>
   );
 }
