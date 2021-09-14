ff2d10b0dc521aea6544972d74782d249ac030aa
Cooper Saye <coopersaye@gmail.com>
2021-09-13 21:09:24 -0700

    Improve projects section styling.

--- components/Projects.js
+++ components/Projects.js
@@ -2,14 +2,19 @@ import Project from './Project.js';
 
 import { projects } from '../data/projects.js';
 
+import styles from '../styles/components/Projects.module.css';
+
 export default function Projects() {
   return (
-    <div>
-      {
-        projects.map((project, i) =>
-          <Project {...project} key={i} />
-        )
-      }
+    <div className={styles.container}>
+      <h1>Projects</h1>
+      <div className={styles.projects}>
+        {
+          projects.map((project, i) =>
+            <Project {...project} key={i} />
+          )
+        }
+      </div>
     </div>
   );
 }
