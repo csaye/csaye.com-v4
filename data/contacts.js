cf5422ff7ee1b26a848547e2671fee6b2fbd412b
Cooper Saye <coopersaye@gmail.com>
2021-09-13 21:00:56 -0700

    Import contact data.

---
+++ data/contacts.js
@@ -0,0 +1,32 @@
+export const contacts = [
+  {
+    icon: <i className="fas fa-envelope icon-blue" />,
+    text: "Email",
+    email: "cooper@saye.co"
+  },
+  {
+    icon: <i className="fab fa-youtube icon-red" />,
+    text: "YouTube",
+    link: "https://www.youtube.com/channel/UCRQa3KuskvvNFz1c-YleSnA",
+  },
+  {
+    icon: <i className="fab fa-github icon-black" />,
+    text: "GitHub",
+    link: "https://github.com/csaye"
+  },
+  {
+    icon: <i className="fab fa-twitter icon-blue" />,
+    text: "Twitter",
+    link: "https://twitter.com/csaye_"
+  },
+  {
+    icon: <i className="fab fa-soundcloud icon-orange" />,
+    text: "SoundCloud",
+    link: "https://soundcloud.com/csaye"
+  },
+  {
+    icon: <i className="fab fa-itch-io icon-red" />,
+    text: "Itch.io",
+    link: "https://csaye.itch.io"
+  }
+];
