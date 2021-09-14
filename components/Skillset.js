ed61ecb288b6e0c0946486d5afa0defdc1b7714d
Cooper Saye <coopersaye@gmail.com>
2021-09-13 20:52:21 -0700

    Display skillset data.

--- components/Skillset.js
+++ components/Skillset.js
@@ -1,34 +1,24 @@
+import Icon from './Icon.js';
+
+import { skillset } from '../data/skillset.js';
+
 export default function Skillset() {
   return (
     <div>
       <h1>Skillset</h1>
       <div>
-        <h1>Games</h1>
-        <p>Unity</p>
-        <p>MonoGame</p>
-        <p>C#</p>
-        <p>Aseprite</p>
-        <p>FL Studio</p>
-        <p>GIMP</p>
-        <p>C++</p>
-        <p>Python</p>
-      </div>
-      <div>
-        <h1>Web</h1>
-        <p>React</p>
-        <p>Next.js</p>
-        <p>JavaScript</p>
-        <p>Vercel</p>
-        <p>Node.js</p>
-        <p>Firebase</p>
-      </div>
-      <div>
-        <h1>Tools</h1>
-        <p>GitHub</p>
-        <p>Trello</p>
-        <p>VSCode</p>
-        <p>Visual Studio</p>
-        <p>Atom</p>
+        {
+          skillset.map((section, i) =>
+            <div key={i}>
+              <h1>{section.title} {section.icon}</h1>
+              {
+                section.icons.map((icon, j) =>
+                  <Icon icon={icon} key={j} />
+                )
+              }
+            </div>
+          )
+        }
       </div>
     </div>
   );
