9de115e3312e86f4ffb0606dc779953a6db02f20
Cooper Saye <coopersaye@gmail.com>
2021-09-15 20:28:10 -0700

    Improve contacts styling.

--- components/Contacts.js
+++ components/Contacts.js
@@ -1,32 +1,37 @@
 import { contacts } from '../data/contacts.js';
 
-export default function Contact() {
+import styles from '../styles/components/Contacts.module.css';
+
+export default function Contacts() {
   return (
-    <div>
-      {
-        contacts.map((contact, i) =>
-          <div key={i}>
-            {
-              contact.email &&
-              <a href={`mailto:${contact.email}`}>
-                {contact.icon}
-                <p>{contact.text}</p>
-              </a>
-            }
-            {
-              contact.link &&
-              <a
-                href={contact.link}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                {contact.icon}
-                <p>{contact.text}</p>
-              </a>
-            }
-          </div>
-        )
-      }
+    <div className={styles.container}>
+      <h1>Contacts</h1>
+      <div className={styles.contacts}>
+        {
+          contacts.map((contact, i) =>
+            <div key={i}>
+              {
+                contact.email &&
+                <a href={`mailto:${contact.email}`}>
+                  {contact.icon}
+                  <p>{contact.text}</p>
+                </a>
+              }
+              {
+                contact.link &&
+                <a
+                  href={contact.link}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {contact.icon}
+                  <p>{contact.text}</p>
+                </a>
+              }
+            </div>
+          )
+        }
+      </div>
     </div>
   );
 }
