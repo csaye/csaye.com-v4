2eba25ace21bbd677685e1fcc0c174732b1ec62b
Cooper Saye <coopersaye@gmail.com>
2021-09-18 23:53:10 -0700

    Improve about section styling.

--- components/About.js
+++ components/About.js
@@ -3,8 +3,10 @@ import styles from '../styles/components/About.module.css';
 export default function About() {
   return (
     <div className={styles.container}>
-      <h1>About</h1>
-      <p>Web and game developer. Fan of open source and creative coding. Working primarily in React, C#, and Python.</p>
+      <h1>Cooper Saye</h1>
+      <h2>web & game developer</h2>
+      <p>Fan of open source and creative coding.</p>
+      <p>Working primarily in React, C#, and Python.</p>
     </div>
   );
 }
