bc198e4bf9a52bb2ab057bb5e693b0c5163ee95e
Cooper Saye <coopersaye@gmail.com>
2021-09-19 00:57:40 -0700

    Improve footer styling.

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,9 @@ import styles from '../styles/components/Footer.module.css';
 export default function Footer() {
   return (
     <div className={styles.container}>
-      <p>&copy; Cooper Saye {new Date().getFullYear()}</p>
+      <div className={styles.skew}>
+        <p>&copy; Cooper Saye {new Date().getFullYear()}</p>
+      </div>
     </div>
   );
 }
