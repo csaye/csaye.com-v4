951d481b8d9a203a0e4dee55ecbec23719669f3e
Cooper Saye <coopersaye@gmail.com>
2021-09-21 19:31:53 -0700

    Create project type selector.

--- components/Projects.js
+++ components/Projects.js
@@ -4,10 +4,30 @@ import { projects } from '../data/projects.js';
 
 import styles from '../styles/components/Projects.module.css';
 
+const defaultTypes = ['game', 'web', 'other'];
+
 export default function Projects() {
   return (
     <div className={styles.container}>
       <h1>Projects</h1>
+      <div className={styles.options}>
+        <div className={styles.types}>
+          {
+            defaultTypes.map((type, i) =>
+              <label
+                className={types.includes(type) ? null : styles.notype}
+                key={i}
+              >
+                <input
+                  type="checkbox"
+                  checked={types.includes(type)}
+                  onChange={e => updateTypes(type, e.target.checked)}
+                />{type}
+              </label>
+            )
+          }
+        </div>
+      </div>
       <div className={styles.projects}>
         {
           projects.map((project, i) =>
