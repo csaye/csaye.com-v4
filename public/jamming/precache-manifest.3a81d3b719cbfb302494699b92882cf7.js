924ff51ff621158f74591f111fbe141d84fafd74
Cooper Saye <coopersaye@gmail.com>
2021-10-27 23:54:51 -0700

    Import Jamming build.

---
+++ public/jamming/precache-manifest.3a81d3b719cbfb302494699b92882cf7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68bf5b7ad36fff9ab8403cc61998114f",
+    "url": "/jamming/index.html"
+  },
+  {
+    "revision": "6fd942f6b3ca96a204b9",
+    "url": "/jamming/static/css/main.a0f08091.chunk.css"
+  },
+  {
+    "revision": "2a3ed650937580cb788d",
+    "url": "/jamming/static/js/2.f29fe41d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/jamming/static/js/2.f29fe41d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fd942f6b3ca96a204b9",
+    "url": "/jamming/static/js/main.8be57feb.chunk.js"
+  },
+  {
+    "revision": "8261c73996e74e0571aa",
+    "url": "/jamming/static/js/runtime-main.60c6d4a8.js"
+  },
+  {
+    "revision": "37be7357a5f8583073df7e0aec787eaa",
+    "url": "/jamming/static/media/background_photo_desktop.37be7357.jpg"
+  }
+]);
