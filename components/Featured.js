55e404d3018d25f397f578a992e2658f33d75885
Cooper Saye <coopersaye@gmail.com>
2021-11-11 23:01:23 -0800

    Improve featured section styling.

--- components/Featured.js
+++ components/Featured.js
@@ -2,10 +2,12 @@ import Project from './Project';
 
 import { projects } from '../data/projects';
 
+import styles from '../styles/components/Featured.module.css';
+
 export default function Featured() {
   return (
-    <div>
-      <div>
+    <div className={styles.container}>
+      <div className={styles.projects}>
         {
           projects
           .filter(project => project.featured)
