5c4885149e13a2c02c73c5dc5f048683ba8d3fa1
Cooper Saye <coopersaye@gmail.com>
2021-11-12 20:43:07 -0800

    Create featured project star.

--- components/Project.js
+++ components/Project.js
@@ -4,10 +4,18 @@ import IconSmall from './IconSmall.js';
 import styles from '../styles/components/Project.module.css';
 
 export default function Project(props) {
-  const { name, description, link, image, buttons, icons, type } = props;
+  const {
+    name, description, link, image, buttons, icons, type, featured
+  } = props;
 
   return (
     <div className={styles.container}>
+      {
+        featured &&
+        <div className={styles.star}>
+          <i className="fas fa-star icon-yellow" />
+        </div>
+      }
       <div className={styles.type}>
         {type === 'web' && <i className="fas fa-laptop icon-blue" />}
         {type === 'game' && <i className="fas fa-dice-five icon-green" />}
