78edbf127338a3362e65d34f1c40d57c6e012ad5
Cooper Saye <coopersaye@gmail.com>
2021-11-16 19:40:04 -0800

    Add award to about section.

--- components/About.js
+++ components/About.js
@@ -5,6 +5,7 @@ export default function About() {
     <div className={styles.container}>
       <h1>Cooper Saye</h1>
       <h2>web & game developer</h2>
+      <h3>Congressional App Challenge 2021 Winner</h3>
       <p>Fan of open source and creative coding.</p>
       <p>Working primarily in React, C#, and Python.</p>
     </div>
