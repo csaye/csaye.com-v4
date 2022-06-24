1c9a9258dabcff3b7df2d066f80b6c28e5d53289
Cooper Saye <coopersaye@gmail.com>
2022-06-24 00:10:20 -0700

    Pass header refs down.

--- pages/index.js
+++ pages/index.js
@@ -11,28 +11,24 @@ import { useRef } from 'react';
 import styles from '../styles/pages/Index.module.css';
 
 export default function Index() {
-  const homeRef = useRef();
   const skillsetRef = useRef();
   const projectsRef = useRef();
 
   // scrolls given section into view
   function scrollTo(section) {
-    if (section === 'home') homeRef.current.scrollIntoView();
+    if (section === 'home') window.scrollTo(0, 0);
     else if (section === 'skillset') skillsetRef.current.scrollIntoView();
     else if (section === 'projects') projectsRef.current.scrollIntoView();
   }
 
   return (
     <div>
-      <span ref={homeRef} />
       <Header scrollTo={scrollTo} />
       <About />
       <Contacts />
       <Featured />
-      <span ref={skillsetRef} />
-      <Skillset />
-      <span ref={projectsRef} className={styles.projectsref} />
-      <Projects />
+      <Skillset skillsetRef={skillsetRef} />
+      <Projects projectsRef={projectsRef} />
       <Footer />
     </div>
   );
