0683cff025651e08d75421235c32a4636cdf31fe
Cooper Saye <coopersaye@gmail.com>
2022-07-01 22:47:15 -0700

    Move link from _app.

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700;900&display=swap" rel="stylesheet" />
       </Head>
       <body>
         <Main />
